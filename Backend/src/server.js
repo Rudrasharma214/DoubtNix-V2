8ca9ea6dbb88602353b6086ebee9ff1d32e44cd3
Rudrasharma214 <rudrashma.810@gmail.com>
2025-12-29 19:54:36 +0530

    feat: Configure production settings for trust proxy and static file serving

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,6 +7,19 @@ const  PORT = env.PORT;
 
 connectDB();
 
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+} 
+
+
 app.listen(PORT, ()=>{
     logger.warn(`Server is running on port ${PORT}`);
 })
